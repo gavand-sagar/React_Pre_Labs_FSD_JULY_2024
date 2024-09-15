d4fea479329da789173dd23740f2e679966882a9
gavand-sagar <gavand.sagar.s@gmail.com>
2024-09-15 14:50:38 +0530

    truthy and falsey

---
+++ Lab-2-truthy-falsey.js
@@ -0,0 +1,15 @@
+let a = {
+
+};
+
+
+// truthy - {},[],"value",'a',-1,2,Infinty,0.4,true
+
+// falsey - null,NaN,0,undefined,"",false
+
+
+if (a) { /// whether the variable 'a' contains any value or not
+    console.log("YES")
+} else {
+    console.log("NO")
+}
