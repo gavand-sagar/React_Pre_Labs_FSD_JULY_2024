882c1dd2e46e1970c56748dccb0e5042f4834cba
gavand-sagar <gavand.sagar.s@gmail.com>
2024-09-15 17:26:33 +0530

    spread and destructure

---
+++ Lab-7-spred.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4]
+let arr2 = [...arr]
+
+arr2.push(5)
+
+console.log(arr2)
+console.log(arr)
---
+++ Lab-8-destructure.js
@@ -0,0 +1,14 @@
+// let array = [3,4,5,6,7]
+
+// let a = array[0]
+// let b = array[1];
+
+// console.log(a,b)
+
+
+let array = [3,6]
+
+let [a, b] = array
+
+console.log(a)
+console.log(b)
