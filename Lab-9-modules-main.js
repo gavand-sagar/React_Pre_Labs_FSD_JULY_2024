a37a3af3a57cda92894e7e331753e8f117caa726
gavand-sagar <gavand.sagar.s@gmail.com>
2024-09-15 17:38:37 +0530

    require() import exports

---
+++ Lab-9-modules-main.js
@@ -0,0 +1,9 @@
+// const { displayInformation, addition, multiply, firstName } = require('./Lab-9-modules-temp.js')
+
+displayInformation();
+
+let result = multiply(6, 7)
+console.log(result);
+
+console.log(firstName)
+
---
+++ Lab-9-modules-temp.js
@@ -0,0 +1,15 @@
+function displayInformation() {
+    console.log("Hello there!!!")
+}
+
+function addition(a, b) {
+    return a + b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+let firstName = "Sagar";
+
+module.exports = { displayInformation, addition, multiply, firstName }
